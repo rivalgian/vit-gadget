8752fff741f4029344526def2f61f20a357b6edb
unknown <rivalgian20@gmail.com>
2026-06-13 23:23:50 +0700

    menu mobile baru

--- script/main.js
+++ script/main.js
@@ -1,10 +1,4 @@
-// Hamburger
-const menuToggle = document.getElementById('menu-toggle');
-const nav = document.querySelector('nav');
 
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
 
 // Swipper
 var swiper = new Swiper(".testimonialSwiper", {
